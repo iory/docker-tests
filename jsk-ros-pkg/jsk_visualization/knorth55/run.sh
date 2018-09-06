41e01f9cbad4f23e8ea3ec314e2feb3ace30a0ce
iory <ab.ioryz@gmail.com>
2018-09-07 01:57:45 +0900

    Add

--- jsk-ros-pkg/jsk_visualization/knorth55/run.sh
+++ jsk-ros-pkg/jsk_visualization/knorth55/run.sh
@@ -4,6 +4,5 @@ xhost +local:
 docker run -it --rm \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        -e DISPLAY=$DISPLAY \
-       iory/knorth55-kinetic-plot-2d
-
+       iory/knorth55-kinetic-plot-2d bash -i -c 'roslaunch jsk_rqt_plugins sample_2dplot.launch'
 xhost -local:
