111d86d28479ea31f17479b756b4a70b10237da5
iory <ab.ioryz@gmail.com>
2018-09-07 02:00:37 +0900

    Add

--- jsk-ros-pkg/jsk_visualization/knorth55/kinetic-hist/run.sh
+++ jsk-ros-pkg/jsk_visualization/knorth55/kinetic-hist/run.sh
@@ -4,6 +4,5 @@ xhost +local:
 docker run -it --rm \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        -e DISPLAY=$DISPLAY \
-       iory/knorth55-kinetic-hist
-
+       iory/knorth55-kinetic-hist bash -i -c 'roslaunch jsk_rqt_plugins sample_histogram.launch'
 xhost -local:
