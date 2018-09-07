a166cf759ec30bfd082ae48dd8fa5d22cf331e93
iory <ab.ioryz@gmail.com>
2018-09-07 23:47:45 +0900

    https://github.com/jsk-ros-pkg/jsk_visualization/pull/666

---
+++ jsk-ros-pkg/jsk_visualization/686/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+xhost +local:root
+docker run -it --rm \
+       --runtime=nvidia \
+       --env="DISPLAY" \
+       --env="QT_X11_NO_MITSHM=1" \
+       --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+       iory/jsk_visualization:685-"$1" bash -i -c 'roslaunch jsk_rviz_plugins polygon_array_sample.launch'
+xhost -local:root
